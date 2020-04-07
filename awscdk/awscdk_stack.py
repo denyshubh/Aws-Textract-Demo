2653d6283e032a18bb2ba335decd66b5c46e65b6
denyshubh <denyshubham@gmail.com>
2020-04-07 15:49:36 +0530

    imported libraries for stack creation ref: https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_lambda_event_sources.html

--- awscdk/awscdk_stack.py
+++ awscdk/awscdk_stack.py
@@ -1,8 +1,24 @@
-from aws_cdk import core
+from aws_cdk import (
+    core as cdk,
+    aws_lambda as _lambda,
+    aws_events as events,
+    aws_iam as iam,
+    aws_sns as sns,
+    aws_sns_subscriptions as snsSubscriptions,
+    aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    aws_s3,
+)
+from aws_cdk.aws_lambda_event_sources import (
+    S3EventSource,
+    SqsEventSource,
+    SnsEventSource,
+    DynamoEventSource,
+)
+from aws_cdk.aws_events_targets import LambdaFunction
 
 
 class AwscdkStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
